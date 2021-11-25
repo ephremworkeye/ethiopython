825cfd522c90ff27ffda1ca519820db8054fc5fa
Ephrem W <ephremworkeye@gmail.com>
2021-11-24 16:00:09 -0800

    Comment model added

--- blog/models.py
+++ blog/models.py
@@ -45,3 +45,20 @@ class Post(models.Model):
                            self.publish.day,
                            self.slug,
                        ])
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    body = models.TextField()
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created_at',)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
