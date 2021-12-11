7918d8c04cd6e9e478e4b743eb13ca672fafba93
Ephrem W <ephremworkeye@gmail.com>
2021-12-10 21:55:09 -0800

    updated the banner image and navbar and added css

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -13,3 +13,11 @@ def total_posts():
 @register.simple_tag
 def total_comments():
     return Comment.objects.count()
+
+
+@register.inclusion_tag('blog/most_commented_posts.html')
+def most_commented_posts(count=3):
+    commented_posts = Post.published.annotate(
+        total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+    return {'commented_posts': commented_posts}
